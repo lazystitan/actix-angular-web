8c432cc0ea8485fa1f6629327b7aedd52cba8f3b
Riton <ritonelion@outlook.com>
2021-01-25 00:27:36 +0800

    complement

---
+++ app/src/app/apiinterceptor.interceptor.spec.ts
@@ -0,0 +1,16 @@
+import {TestBed} from '@angular/core/testing';
+
+import {APIInterceptorInterceptor} from './apiinterceptor.interceptor';
+
+describe('APIInterceptorInterceptor', () => {
+    beforeEach(() => TestBed.configureTestingModule({
+        providers: [
+            APIInterceptorInterceptor
+        ]
+    }));
+
+    it('should be created', () => {
+        const interceptor: APIInterceptorInterceptor = TestBed.inject(APIInterceptorInterceptor);
+        expect(interceptor).toBeTruthy();
+    });
+});
