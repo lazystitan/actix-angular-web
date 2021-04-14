9429a70b444141392bb5800d976737c57173a1b2
Riton <ritonelion@outlook.com>
2021-04-15 01:39:15 +0800

    simple logger

---
+++ src/logger.rs
@@ -0,0 +1,53 @@
+use std::env;
+use chrono::prelude::*;
+use log4rs;
+use std::io::ErrorKind;
+
+struct Logger<'a> {
+    name : &'a str
+}
+
+impl<'a> Logger<'a> {
+    fn init_config() {
+        log4rs::init_file("config/log4rs.yaml", Default::default()).unwrap_or_else(|e| {
+            panic!("init logger file failed: {:?}", e);
+        });
+    }
+    fn new(name : &'a str) -> Self {
+        Self { name }
+    }
+    fn trace(&self, from : &str, message : &str) {
+        trace!("{} from {} with {}",self.name, from, message)
+    }
+
+    fn info(&self, from : &str, message : &str) {
+        info!("{} from {} with {}",self.name, from, message)
+    }
+
+    fn warn(&self, from : &str, message : &str) {
+        warn!("{} from {} with {}",self.name, from, message)
+    }
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+
+    #[test]
+    fn logger_test() {
+        trace!("This is trace");
+        info!("This is info");
+        warn!("This is warn");
+    }
+
+    #[test]
+    fn logger_log() {
+        Logger::init_config();
+        let user_logger = Logger::new("user");
+        user_logger.trace("user", &format!("{} log in", "riton"));
+        user_logger.info("user", "cannot find user");
+        user_logger.warn("user", &format!("cannot find user {} password", "riton"));
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 #[macro_use]
 extern crate diesel;
 extern crate derive_more;
+#[macro_use]
+extern crate log;
+// extern crate log4rs;
 
 pub mod schema;
 pub mod models;
 pub mod db;
 pub mod api;
 pub mod error;
+pub mod logger;
 
 use actix_web::{App, HttpServer};
 
