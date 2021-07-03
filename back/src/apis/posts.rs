62a6a2b8128b6cb06dc5f2c01c4ba2cae5dfc536
Riton <ritonelion@outlook.com>
2021-07-03 20:06:11 +0800

    replace error

--- back/src/apis/posts.rs
+++ back/src/apis/posts.rs
@@ -1,18 +1,20 @@
-use actix_web::{get, post, web, HttpResponse, HttpRequest, Result as AResult};
 use crate::db;
 use crate::error::CustomError;
 use crate::models::PostInsert;
+use actix_web::{get, post, web, HttpRequest, HttpResponse, Result as AResult};
 
 #[get("/posts")]
 pub async fn posts(data_service: web::Data<db::DataService>) -> AResult<HttpResponse, CustomError> {
     info!("get posts");
-    let posts_result = web::block(move || data_service.get_posts()).await.map_err(|e| {
-        eprintln!("{:?}", e);
-        HttpResponse::InternalServerError().finish()
-    });
+    let posts_result = web::block(move || data_service.get_posts())
+        .await
+        .map_err(|e| {
+            eprintln!("{:?}", e);
+            CustomError::InternalError("Internal error".to_string())
+        });
     match posts_result {
         Ok(posts) => Ok(HttpResponse::Ok().json(posts)),
-        Err(error) => Ok(error),
+        Err(error) => Err(error),
     }
 }
 
@@ -25,35 +27,28 @@ pub async fn post(
         .await
         .map_err(|e| {
             eprintln!("{:?}", e);
-            HttpResponse::InternalServerError().finish()
+            CustomError::InternalError("Internal error".to_string())
         });
     match post {
         Ok(post) => Ok(HttpResponse::Ok().json(post)),
-        Err(error) => Ok(error),
+        Err(error) => Err(error),
     }
 }
 
 #[post("/post")]
-pub async fn add_post(req: HttpRequest,data_service: web::Data<db::DataService>, form: web::Json<PostInsert>) -> AResult<HttpResponse> {
+pub async fn add_post(
+    req: HttpRequest,
+    data_service: web::Data<db::DataService>,
+    form: web::Json<PostInsert>,
+) -> AResult<HttpResponse, CustomError> {
     if let Some(token) = req.headers().get("Authorization") {
         if let Ok(_) = data_service.validate_token(token.to_str().unwrap()) {
             let post_insert = form.0;
             let res = data_service.add_post(post_insert);
             if let Ok(_) = res {
-                return Ok(HttpResponse::Ok().body("{\"code\":0}"))
+                return Ok(HttpResponse::Ok().body("{\"code\":0}"));
             }
         }
-
     }
-    Ok(HttpResponse::Ok().body("{\"code\":1}"))
-}
-
-#[post("/post_form")]
-pub async fn add_post_form(data_service: web::Data<db::DataService>, form: web::Form<PostInsert>) -> AResult<HttpResponse> {
-    let post_insert = form.0;
-    let res = data_service.add_post(post_insert);
-    match res {
-        Ok(_) => Ok(HttpResponse::Ok().body("ok")),
-        Err(_) => Ok(HttpResponse::Ok().body("nok"))
-    }
+    Err(CustomError::BadClientData("Validation failed".to_string()))
 }
