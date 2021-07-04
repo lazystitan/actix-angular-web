1f4f15b2ae0fad950a6932877cd16c5a5fb3c461
Riton <ritonelion@outlook.com>
2021-07-04 23:59:39 +0800

    front delete post

--- app/src/app/components/post-card/post-card.component.ts
+++ app/src/app/components/post-card/post-card.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit, Input} from '@angular/core';
 
 import {Post} from '../../model/post'
+import {TokenStorageService} from "../../service/auth/token-storage.service";
+import {PostService} from "../../service/posts/post.service";
 
 @Component({
   selector: 'app-post-card',
@@ -18,10 +20,24 @@ export class PostCardComponent implements OnInit {
     last_update_time: ''
   };
 
-  constructor() {
+  constructor(
+    public tokenService: TokenStorageService,
+    private postService: PostService
+  ) {
   }
 
   ngOnInit(): void {
+  }
+
+  deletePost(id : number) {
+    this.postService.deletePost(id).subscribe(data => {
+      if (data.code == 0) {
+        alert('删除成功！');
+        window.location.reload();
+      } else {
+        alert(data.message);
+      }
+    })
   }
 
 }
--- app/src/app/service/posts/post.service.ts
+++ app/src/app/service/posts/post.service.ts
@@ -15,24 +15,29 @@ export class PostService {
   ) { }
 
   private log(message: string) {
-    this.messageService.add(`PostService: ${message}`)
+    this.messageService.add(`PostService: ${message}`);
   }
 
   private postsUrl = 'posts';
   private postUrl = 'post';
 
   getPosts(): Observable<Post[]> {
-    this.log('PostService: fetched posts')
-    return this.http.get<Post[]>(this.postsUrl)
+    this.log('PostService: fetched posts');
+    return this.http.get<Post[]>(this.postsUrl);
   }
 
   getPost(id:Number): Observable<Post> {
     this.messageService.add(`PostService: fetched post id=${id}`);
-    return this.http.get<Post>(this.postUrl + `/${id}`)
+    return this.http.get<Post>(this.postUrl + `/${id}`);
   }
 
   addPost(post: PostInsert): Observable<any> {
     this.messageService.add(`PostService: add post`);
-    return this.http.post<PostInsert>(this.postUrl, post)
+    return this.http.post<PostInsert>(this.postUrl, post);
+  }
+
+  deletePost(post_id: number): Observable<any> {
+    this.messageService.add(`PostServcie: delete post ${post_id}`);
+    return this.http.delete<number>(this.postUrl + `/${post_id}`);
   }
 }
